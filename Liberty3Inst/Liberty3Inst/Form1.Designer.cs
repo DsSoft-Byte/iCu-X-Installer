3803a17a9b8dbcb7530e8560d48e63784b3be487
Daniel <djarocki7@gmail.com>
2023-12-14 21:29:28 +0000

    Add Files

---
+++ Liberty3Inst/Liberty3Inst/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Liberty3Inst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to the Online iOSBridge Installer!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(14, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(691, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please Note: A internet connection is needed trough the whole progress, this will" +
+    " download roughly 100MB Of Programm Data";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 384);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(788, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.checkBox1.Location = new System.Drawing.Point(17, 352);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(284, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Add iOSBridge and the plugin installer to System PATH";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(17, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(788, 100);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Install Now\r\nThis will install the newest version from GitHub on your System";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(17, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(788, 100);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Uninstall\r\nThis will uninstall the whole iOSBridge from your System, with no left" +
+    "overs";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(555, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Installation Progress wil be displayed in green below";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(369, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Installer V1.0.3";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(845, 419);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DsSoft iOSBridge Insatller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Liberty3Inst/Liberty3Inst/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace Liberty3Inst
+{
+    public partial class Form1 : Form
+    {
+        WebClient webClient = new WebClient();
+        string zipFile = @"C:\iCuPlus.zip"; // Path to save the downloaded zip file
+        string extractPath = @"C:\";
+        public Form1()
+        {
+            InitializeComponent();
+            webClient.DownloadProgressChanged += DownloadProgressChanged;
+            webClient.DownloadFileCompleted += DownloadCompleted;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DownloadProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update progress bar or any visual indicator of download progress
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DownloadAndInstallProgram();
+        }
+        private void DownloadAndInstallProgram()
+        {
+            try
+            {
+                webClient.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/DsSoft-Byte/iCu-X/main/iCures.zip"), zipFile);
+                button1.Enabled = false; // Disable the button during download
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage; // Update progress bar with download progress
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UninstallProgram();
+        }
+
+        private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            button1.Enabled = true; // Enable the button after download completes
+            if (e.Error == null)
+            {
+                try
+                {
+                    // Extract the contents of the ZIP file directly to C:\
+                    System.IO.Compression.ZipFile.ExtractToDirectory(zipFile, extractPath);
+
+                    if (checkBox1.Checked)
+                    {
+                        // Add C:\iCures\MainProgramm\ to the system PATH environment variable
+                        string newPath = Path.Combine(extractPath, "iCures\\MainProgramm");
+                        string pathVariable = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+
+                        if (!pathVariable.Contains(newPath))
+                        {
+                            pathVariable += ";" + newPath;
+                            Environment.SetEnvironmentVariable("PATH", pathVariable, EnvironmentVariableTarget.Machine);
+                        }
+                    }
+
+                    MessageBox.Show("Installation completed successfully!");
+                    File.Delete(zipFile);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error installing: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error downloading file: " + e.Error.Message);
+            }
+        }
+        private void UninstallProgram()
+        {
+            string icuresFolder = @"C:\iCures";
+            string zipFile = @"C:\iCuPlus.zip";
+
+            try
+            {
+                // Check if the iCures folder exists before attempting deletion
+                if (Directory.Exists(icuresFolder))
+                {
+                    // Delete the iCures folder and its contents recursively
+                    Directory.Delete(icuresFolder, true);
+                }
+
+                // Check if the iCuPlus.zip file exists before attempting deletion
+                if (File.Exists(zipFile))
+                {
+                    // Delete the iCuPlus.zip file
+                    File.Delete(zipFile);
+                }
+
+                MessageBox.Show("Uninstallation completed successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during uninstallation: " + ex.Message);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Installer Version V1.0.3, Built on our DsSoft Liberty3 Installer framework. Feel free to take this Installer" +
+                "change it according to your needs and use it for your own projects. Credit me or DsSoft then though.");
+        }
+    }
+}
